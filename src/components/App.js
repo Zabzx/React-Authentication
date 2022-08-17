70ae2d7551a06d35043c29c0d98710f01b36c600
Ziabeher Phillips <zabq02@gmail.com>
2022-08-17 13:52:38 -0400

    React router setup

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,21 @@ import React from "react";
 import Signup from "./Signup";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../context/AuthContext";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <AuthProvider>
     <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
       <div className="w-100" style={{ maxWidth: "400px" }}>
-      <Signup/>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
       </div>
     </Container>
     </AuthProvider>
+    </BrowserRouter>
   );
 }
 
