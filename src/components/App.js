ea5a8359f086e0788ec941b1c5f78a0c262a3a75
Ziabeher Phillips <zabq02@gmail.com>
2022-08-18 22:43:51 -0400

    Authentication complete

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Dashboard from "./Dashboard";
 import Login from "./Login";
 import PrivateRoute from "./PrivateRoute";
 import ForgotPassword from "./ForgotPassword";
+import UpdateProfile from "./UpdateProfile";
 
 function App() {
   return (
@@ -19,6 +20,11 @@ function App() {
         <Route path="/" element={
           <PrivateRoute>
             <Dashboard />
+          </PrivateRoute>
+        } />
+        <Route path="/update-profile" element={
+          <PrivateRoute>
+            <UpdateProfile />
           </PrivateRoute>
         } />
         <Route path="/login" element={<Login />} />
